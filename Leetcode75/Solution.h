f47f8fbd1b1e3db1a8ee58b64a4e574150b89ec1
Abdul <muiz.rpi@gmail.com>
2021-01-06 02:43:06 -0500

    Add project files.

---
+++ Leetcode75/Solution.h
@@ -0,0 +1,13 @@
+#ifndef SOLUTON_H_
+#define SOLUTION_H_
+
+#define IMPLEMENTED 0x10
+#define NOT_IMPLEMENTED 0x09
+
+class Soluton {
+public:
+	int mySolution() { return NOT_IMPLEMENTED; }
+	int optimalSolution() { return NOT_IMPLEMENTED; }
+};
+
+#endif
---
+++ Leetcode75/main.cpp
@@ -0,0 +1,22 @@
+﻿#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+/**
+* Leetcode75
+* 
+* ✔ Project Goal: Go through the list of 75 leetcode problems.
+* (https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-100-LeetCode-Questions-to-Save-Your-Time-OaM1orEU)
+* ✔ Properly document the process used to solve the problem.
+*/
+
+int main(int argc, char* argv) {
+
+	int problem_number;
+	printf("Welcome to Leetcode75\nEnter the problem number to run: ");
+	std::cin >> problem_number;
+
+	printf("Problem: %d\n", problem_number);
+
+	return EXIT_SUCCESS;
+}
